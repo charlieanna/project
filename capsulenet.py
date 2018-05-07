dbffa68475cfecda9361d9e9e33da7b95a082797
Ankur <ankothari@gmail.com>
2018-05-06 21:11:07 -0400

    adds reuters code

--- capsulenet.py
+++ capsulenet.py
@@ -144,13 +144,12 @@ def load_imdb(maxlen=400):
     from keras.datasets import imdb
 
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-    # return (x_train, y_train), (x_test, y_test)
-
+    # (x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                            # test_split=0.2)
+    # x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    # x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
     print('Loading data...')
-    (x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                             test_split=0.2)
+   
     print(len(x_train), 'train sequences')
     print(len(x_test), 'test sequences')
 
