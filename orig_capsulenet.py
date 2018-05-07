9d3173c6e47623ba41eaa4863eade828488fc2dd
Ankur <ankothari@gmail.com>
2018-05-07 00:49:42 -0400

    adds reuters

--- orig_capsulenet.py
+++ orig_capsulenet.py
@@ -132,9 +132,9 @@ def test(model, data):
 
 
 def load_imdb(maxlen=400):
-    from keras.datasets import imdb
-
-    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+    from keras.datasets import reuters
+    (x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_featuresx_words)
+   # (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
     x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
     x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
     return (x_train, y_train), (x_test, y_test)
